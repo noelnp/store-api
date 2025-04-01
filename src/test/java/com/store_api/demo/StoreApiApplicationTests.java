556c26c8658ffc0fcdcb4e6c0f8f4d84b95ca5c6
noelnp <noel.peltomaki@gmail.com>
2025-04-01 11:06:52 +0200

    Initial commit

---
+++ src/test/java/com/store_api/demo/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
